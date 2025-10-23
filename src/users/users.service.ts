9e06fd7349478434aa0e351655e0e467be234f90
unknown <lmparanal@gbox.ncf.edu.ph>
2025-10-23 13:21:35 +0800

    Implemented user service functions and fixed /users endpoints

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,15 +5,6 @@ import { RowDataPacket, ResultSetHeader } from 'mysql2';
 
 @Injectable()
 export class UsersService {
-  deleteUser(arg0: number) {
-    throw new Error('Method not implemented.');
-  }
-  updateUser(arg0: number, body: any) {
-    throw new Error('Method not implemented.');
-  }
-  getAll() {
-    throw new Error('Method not implemented.');
-  }
   constructor(private readonly db: DatabaseService) {}
 
   private pool = () => this.db.getPool();
